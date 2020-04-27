752cf40b110a51c180c50ca81734b219915c165b
Katrina Pritchard <pritckat@gmail.com>
2020-04-27 19:04:58 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,11 +2,14 @@
 def position_taken?(board, index)
   if board[index] == "" || " " || nil
     return false
-  #elsif board[index] == " "
-    #return false
-  #elsif board[index] == nil
-    #return false
-  elsif board[index] == "X" || "O"
+  else
     return true
+  # elsif board[index] == " "
+  #   return false
+  # elsif board[index] == nil
+  #   return false
+  # elsif board[index] == "X" || "O"
+  #   return true
+
   end
 end
